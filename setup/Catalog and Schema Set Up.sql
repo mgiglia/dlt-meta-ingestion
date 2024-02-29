daf6ecb35c8ff70606007634254bdbb9c6bcb5f1
mgiglia <mgiglia>
2024-02-29 01:16:50 +0000

    added set up folder and a notebook to create the required catalog, schemas and volumes for the project.

---
+++ setup/Catalog and Schema Set Up.sql
@@ -0,0 +1,29 @@
+-- Databricks notebook source
+-- MAGIC %python
+-- MAGIC dbutils.widgets.dropdown("env_mode", "dev", ("dev", "tst", "prd"), "Environment")
+
+-- COMMAND ----------
+
+create catalog if not exists lakehouse_${env_mode};
+
+-- COMMAND ----------
+
+use catalog lakehouse_${env_mode};
+
+-- COMMAND ----------
+
+select current_catalog();
+
+-- COMMAND ----------
+
+create schema if not exists landing;
+
+-- COMMAND ----------
+
+use schema landing;
+
+-- COMMAND ----------
+
+drop volume if exists dropbox;
+create external volume dropbox
+location 's3://s3-databricks-mgiglia-solutions-landing/${env_mode}/dropbox';
